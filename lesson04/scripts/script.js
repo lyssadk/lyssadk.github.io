e461dd15d46128e397adbbeb2084385ca2f2ef64
lyssadk <adkucharyski@gmail.com>
2021-05-15 19:45:34 -0600

    l4

--- lesson04/scripts/script.js
+++ lesson04/scripts/script.js
@@ -7,10 +7,30 @@ window.addEventListener('load', () => {
    
     // To solve the mid resiszing issue with responsive class on
     //window.onresize = () => {}
-        const lu = document.querySelector('#datetime');
-        lu.textContent = document.lastModified;
-    
-        const cry = document.querySelector('#copyright-year');
-        cry.textContent = new Date().getFullYear();
+
+    const week = document.querySelector('#dayweek');
+    var date = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    week.textContent = weekday[date.getDay()];
+
+    const dayNum = document.querySelector('#day');
+    dayNum.textContent = date.getDate();
+
+    var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthNum = document.querySelector('#month');
+    monthNum.textContent = monthName[date.getMonth()];
+
+    const year = document.querySelector('#year');
+    year.textContent = date.getFullYear();
+
+    const cry = document.querySelector("#current-year");
+    cry.textContent = date.getFullYear();
     
 });
