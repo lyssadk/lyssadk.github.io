8663b649a4ccf3efe3259c49bdc3fae9969b33b4
lyssadk <adkucharyski@gmail.com>
2021-05-22 13:52:51 -0600

    alertbox

--- lesson05/scripts/script.js
+++ lesson05/scripts/script.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
 
     //anonymous function- without a name and you'll only use once but no where else
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-   
+    
     // To solve the mid resiszing issue with responsive class on
     //window.onresize = () => {}
 
