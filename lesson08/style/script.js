9256eab1f24788f8d0e5d7ab0912452267a6e4c7
lyssadk <adkucharyski@gmail.com>
2021-06-12 21:52:50 -0600

    small

--- lesson08/style/script.js
+++ lesson08/style/script.js
@@ -1,3 +1,18 @@
+
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+window.addEventListener('load', () => {
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    //anonymous function- without a name and you'll only use once but no where else
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+    
+    WebFont.load({
+        google: {
+          families: [
+            'Oswald'
+          ]
+        }});
+});
