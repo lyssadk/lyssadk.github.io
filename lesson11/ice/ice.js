c4b97dcae3876d7909267b0d0a98ebf599488380
lyssadk <adkucharyski@gmail.com>
2021-07-02 12:04:49 -0600

    ice

--- lesson11/ice/ice.js
+++ lesson11/ice/ice.js
@@ -4,17 +4,23 @@ fetch(apiURL)
   .then((events) => {
     console.log(events);
 
-    
+    let months = [ "January", "February", "March", "April", "May", "June", 
+           "July", "August", "September", "October", "November", "December"];
+
     for(let i = 0; i < events.length; i++){
         
         let eventSection = document.createElement('section');
         let dateDiv = document.createElement('div');
+        dateDiv.classList.add('date');
         let eventDiv = document.createElement('div');
         let time = document.createElement('p');
-        let type = document.createElement('i');
+        let type = document.createElement('p');
         let eventName = document.createElement('h1');
         let tagDiv = document.createElement('div');
-        
+        let day = document.createElement('h1');
+        let month = document.createElement('p');
+        let year = document.createElement('p');
+        let organizer = document.createElement('p');
         eventName.textContent = events[i].properties.name
         for (let tagCount = 0; tagCount < events[i].tags.length; tagCount++){
             let tag = document.createElement('p');
@@ -22,12 +28,27 @@ fetch(apiURL)
             tagDiv.appendChild(tag);
             tag.setAttribute('class', events[i].tags[tagCount].split(" ").join(""));
         }
-        
+        organizer.textContent = "Organized by: " + events[i].properties.organized_by;
+        for (let k = 0; k < tagDiv.length; k++){
+            if(tag[k].textContent == "Event Over"){
+                organizer.textContent = "";
+                
+            }
+            console.log(tag[k].textContent);
+        }
+
         //Start Time and End Time
-        time.textContent = "PLACEHOLDER TIME";
-        const date = new Date(events[i].properties.start)
-        dates = date.toDateString()
-        console.log(dates)
+        let startdate = new Date(events[i].properties.start);
+        let enddate = new Date(events[i].properties.end);
+        day.textContent = startdate.getDate();
+        year.textContent = startdate.getFullYear();
+        mon = startdate.getMonth();
+        month.textContent = months[mon];
+        
+        time.innerHTML = 	"&#128337 Time: " + startdate.toLocaleTimeString().substring(0,5) + startdate.toLocaleTimeString().substr(8,8) + '-' + enddate.toLocaleTimeString().substring(0,5) + enddate.toLocaleTimeString().substr(8,8);
+        
+         
+        
         
         type.textContent = "Event Type: " + events[i].type;
         
@@ -35,7 +56,13 @@ fetch(apiURL)
         eventSection.appendChild(eventDiv);
         eventDiv.appendChild(tagDiv);
         eventDiv.appendChild(eventName);
+        eventDiv.appendChild(time);
         eventDiv.appendChild(type);
+        eventDiv.appendChild(organizer);
+        dateDiv.appendChild(month)
+        dateDiv.appendChild(day);
+        dateDiv.appendChild(year);
+        
         
         document.querySelector("#content").appendChild(eventSection);
         
