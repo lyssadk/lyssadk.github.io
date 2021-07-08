7a0ac258a62144750c4a2ca44ad66302450f67ce
lyssadk <adkucharyski@gmail.com>
2021-07-08 13:26:03 -0600

    temple stuff

--- temple-inn/temple-js/temple.js
+++ temple-inn/temple-js/temple.js
@@ -1,14 +1,34 @@
 window.addEventListener('load', () => {
-    const hambutton = document.querySelector('.ham');
-    const mainnav = document.querySelector('#navigation');
-
-    //anonymous function- without a name and you'll only use once but no where else
-    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-    
     WebFont.load({
         google: {
           families: [
             'Oswald'
           ]
-        }});
+        }}); 
+    const requestUTL = 'json/temple.json'
+    fetch(requestUTL)
+      .then((response) =>{
+          return response.json();
+      })
+      .then((jsonObject) =>{
+          console.log(jsonObject);
+           Object.entries(jsonObject).forEach(([key,temple])=>{
+          //    if (temple.state == 'ID'){
+             buildTempleCard(temple);
+          // }
+          });
+      });
 });
+
+function buildTempleCard(temple){
+  console.log(temple);
+  let card = document.createElement('section');
+  card.classList.add("temple");
+  card.innerHTML = `
+                    <img src = "${temple.imageurl}" alt="${temple.name}">
+                    <h2>${temple.name}</h2>  
+                    <p>First President: <b>${temple.presidents[0]}</b></p>
+                    <p>Current President: <b>${temple.presidents[temple.presidents.length-1]}</b></p>`;
+  document.querySelector("#temple-card").appendChild(card);
+
+}
