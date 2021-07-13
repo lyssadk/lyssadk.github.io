b7d2bbb4065b530b58a9932b23ea1cc234ffb6eb
lyssadk <adkucharyski@gmail.com>
2021-07-12 21:58:39 -0600

    temple

--- temple-inn/temple-js/temple.js
+++ temple-inn/temple-js/temple.js
@@ -1,4 +1,11 @@
 window.addEventListener('load', () => {
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    //anonymous function- without a name and you'll only use once but no where else
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+    
+  
     WebFont.load({
         google: {
           families: [
