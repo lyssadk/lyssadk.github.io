f318c7945a2f5a4edb5d88e56e33e0784e99ff3f
lyssadk <adkucharyski@gmail.com>
2021-07-17 15:17:43 -0600

    form stuff and five day

--- temple-inn/temple-js/temple.js
+++ temple-inn/temple-js/temple.js
@@ -76,10 +76,75 @@ function buildTempleCard(temple){
 
   card.appendChild(contact);
 
-  document.querySelector("#temple-card").appendChild(card);
-}
+  document.querySelector("#temple-card").appendChild(card);}
 
-function closures(temple){
+
+
  
+  let requestUTL = 'https://api.openweathermap.org/data/2.5/forecast?zip=85296,us&appid=257d32dd0450e7d16d8ba54383f7bbce&units=imperial';
+  fetch(requestUTL)
+      .then((response) =>{
+          return response.json();
+      })
+      .then((weather) =>{
+        console.log(weather)
+        /////////////////////////////
+/*five day forecast stuff */
+////////////////////////////
+// filtering the array for temps at 6:00pm
+okey = weather.list 
+filter = okey.filter(day => day.dt_txt.includes('18:00:00'))
 
-}
+      for (let i = 0 ; i < filter.length ; i++){
+       
+        // creating html elements / adding classes/srcs
+        let card = document.createElement('div'); 
+        card.classList.add('mi')
+        
+        let day = document.createElement('h3');
+
+        let icon  = document.createElement('img');
+        let iconsrc = filter[i].weather[0].icon;
+        icon.src = `http://openweathermap.org/img/wn/${iconsrc}.png`
+        let temp = document.createElement('p');
+
+        // content from api
+        temp.textContent = Math.round( filter[i].main.temp) + '°';
+         
+        // the day stuff, im sure there is an easier way to do this, just ran out of time to figure it out so i did this haha
+          var result = new Date();
+          var weekdays = new Array(12);
+          weekdays[0] = "Sun";
+          weekdays[1] = "Mon";
+          weekdays[2] = "Tues";
+          weekdays[3] = "Wed";
+          weekdays[4] = "Thurs";
+          weekdays[5] = "Fri";
+          weekdays[6] = "Sat";
+          weekdays[7] = "Sun";
+          weekdays[8] = "Mon";
+          weekdays[9] = "Tues";
+          weekdays[10] = "Wed";
+          weekdays[11] = "Thurs";
+          weekdays[12] = "Fri";
+          ok = result.getDay();
+
+          // literally don't ask me. this is really rough js i used that def could've been simplified. again, it works but it ain't the most efficient
+          switch (ok) {
+            case 6:
+              day.textContent = weekdays[ok + (i - 6)]
+              break;
+            default:
+              day.textContent = weekdays[ok +(i + 1)]
+              break;
+          }
+          // 
+
+          // adding stuff to page
+          card.appendChild(day);
+          card.appendChild(icon);
+          card.appendChild(temp);
+          document.querySelector('#five-day').appendChild(card);
+      }
+      ////////////////////
+      });
